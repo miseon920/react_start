15a7314dfbe2c0783c50386b5e54e26f35b08765
miseon <miseon920@naver.com>
2022-08-26 11:10:08 +0900

    20220826

--- react_start00/src/App.js
+++ react_start00/src/App.js
@@ -1,16 +1,30 @@
 import Tab from "./pages/Tab01";
 import MainVisual from "./pages/MainVisual";
-import './basic.css'
-import SyncSlick from './pages/SyncSlick'
+import "./basic.css";
+import SyncSlick from "./pages/SyncSlick";
+import MainSlider from "./pages/SlickCustom";
+
+// const App = () => {
+//   return (
+//     <>
+//       <h2>기본슬릭</h2>
+//       <MainVisual />
+//       <hr />
+//       <h2>슬릭두개연결</h2>
+//       <SyncSlick />
+//       <hr />
+//       <h2>슬릭최종</h2>
+//       <SlickCustom />
+//     </>
+//   );
+// };
 
 const App = () => {
-    return (
-        <>
-            <MainVisual />
-            <hr />
-            <SyncSlick />
-        </>
-    )
-}
-
+  return (
+    <>
+      <img src="./img/mainvisual01.jpg" alt="" />
+      <MainSlider />
+    </>
+  );
+};
 export default App;
---
+++ react_start00/src/pages/SlickCustom.js
@@ -0,0 +1,94 @@
+import { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+
+/**
+ *
+ * @returns 두개합침
+ */
+const SlickCustom = () => {
+  const SlideEle = [
+    {
+      id: 1,
+      h2: "01.JEWELLERY COLLECTION",
+      p: "모던하면서도 클래식한 디자인들을 완벽하게 표현 그라프 하우스의 시그너쳐 컬렉션은 다양한 컷, 스톤 그리고 색상들을 멋진 조합으로 선사합니다.",
+    },
+    {
+      id: 2,
+      h2: "02.JEWELLERY COLLECTION",
+      p: "모던하면서도 클래식한 디자인들을 완벽하게 표현 그라프 하우스의 시그너쳐 컬렉션은 다양한 컷, 스톤 그리고 색상들을 멋진 조합으로 선사합니다.",
+    },
+    {
+      id: 3,
+      h2: "03.JEWELLERY COLLECTION",
+      p: "모던하면서도 클래식한 디자인들을 완벽하게 표현 그라프 하우스의 시그너쳐 컬렉션은 다양한 컷, 스톤 그리고 색상들을 멋진 조합으로 선사합니다.",
+    },
+  ];
+
+  const S1 = useRef();
+  const [num, setNum] = useState();
+
+  //로드 후 최초 실행 - useEffct / useState 초기값을 0으로 - render와 update 가 한번 실행하게 할때
+  useEffect(() => {
+    //setTimeOut 등등, 초기값 세팅이라고 보면됨
+    setNum(0);
+  }, []); //[]은 의존성배열 / 안쓰면 업데이트할떄마다 계속진행하므로 [변수]를 쓰면 변수가 바뀔때마다 실행함
+  const set = {
+    arrows: false,
+    afterChange: (idx) => {
+      //인덱스 받아오기! / 리액트는 매개변수가 하나임
+      //console.log(idx); 값이 바뀔때마다 받아와야하므로 state
+      setNum(idx);
+    },
+  };
+  //slick - slickGoTo, index
+  return (
+    <>
+      <Slider ref={S1} {...set} className="slick_s">
+        {/*컴포넌트를 직접 제어하기위해서 이름을 붙임, ref={S1} */}
+        {/* <figure>01</figure>
+        <figure>02</figure>
+        <figure>03</figure> */}
+        {/*배열로 넣기 */}
+        {SlideEle.map((item, idx) => (
+          <figure
+            key={item.id}
+            className={`item0${idx + 1} ${idx == num ? "on" : ""}`}
+          >
+            {item.id}
+          </figure>
+          //key={item.id} 은 map의 매개변수인 idx로 쓸수 있으나 성능저하 및 데이터의 문제가 있으므로 배열의 id로 준다
+        ))}
+      </Slider>
+      {/*번호받기 */}
+      <div className="num">
+        {num + 1} / {SlideEle.length}
+      </div>
+      <div className="slideArrow">
+        {/*S1.current = Slider */}
+        <i className="xi-arrow-left" onClick={() => S1.current.slickPrev()}></i>
+        <i
+          className="xi-arrow-right"
+          onClick={() => S1.current.slickNext()}
+        ></i>
+      </div>
+
+      <ul className="slideDot">
+        {SlideEle.map((dots, idx) => (
+          <li key={dots.id} className={idx == num ? "on" : ""}>
+            <a
+              href=""
+              onClick={(e) => {
+                S1.current.slickGoTo(idx);
+                e.preventDefault();
+              }}
+            >
+              {dots.id}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default SlickCustom;
